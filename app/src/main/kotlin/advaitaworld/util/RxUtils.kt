a43ab958f9f4ae28390d33e9be49b274c55e8689
Dmitry Suzdalev <dimsuz@gmail.com>
2015-02-06 14:06:40 +0200

    Warn if task is started on main thread

--- app/src/main/kotlin/advaitaworld/util/RxUtils.kt
+++ app/src/main/kotlin/advaitaworld/util/RxUtils.kt
@@ -2,14 +2,23 @@ package advaitaworld.util
 
 import rx.Subscriber
 import rx.Observable
-import rx.functions.Action1
 import timber.log.Timber
+import android.os.Looper
+import advaitaworld.BuildConfig
 
 public fun <T> runOnce(body: () -> T) : Observable<T> {
-    return Observable.create({ subscriber ->
+    var observable = Observable.create<T>({ subscriber ->
         // need to wrap in extra fun because Kotlin does not allow local returns from lambdas
         runBodyOnce(body, subscriber)
     })
+    if(BuildConfig.DEBUG) {
+        observable = observable.doOnSubscribe({
+            if (Looper.myLooper() == Looper.getMainLooper()) {
+                Timber.e("note: task will be running on ui thread! this might be an error")
+            }
+        })
+    }
+    return observable
 }
 
 private fun <T> runBodyOnce(body: () -> T, subscriber: Subscriber<T>?) {
