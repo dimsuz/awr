12caf9ee500477092a013bbc5bb1644577f01847
Dmitry Suzdalev <dimsuz@gmail.com>
2015-02-07 02:20:18 +0200

    Start laying out a components of PostActivity

--- app/src/main/kotlin/advaitaworld/PostActivity.kt
+++ app/src/main/kotlin/advaitaworld/PostActivity.kt
@@ -7,6 +7,10 @@ import rx.android.lifecycle.LifecycleObservable
 import advaitaworld.util.printError
 import timber.log.Timber
 import advaitaworld.support.RxActionBarActivity
+import android.view.Menu
+import android.support.v7.widget.Toolbar
+import advaitaworld.parsing.PostData
+import android.widget.TextView
 
 public class PostActivity : RxActionBarActivity() {
     val server: Server by ServerProvider()
@@ -14,15 +18,32 @@ public class PostActivity : RxActionBarActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post)
+        setSupportActionBar(findViewById(R.id.toolbar) as Toolbar)
+        getSupportActionBar().setDisplayShowTitleEnabled(false)
         val postId = getIntent().getStringExtra(EXTRA_POST_ID)
         if(postId == null) {
             throw RuntimeException("post id missing")
         }
+        Timber.d("start full post getter")
         LifecycleObservable.bindActivityLifecycle(lifecycle(), server.getFullPost(postId))
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
-                        { Timber.d("got full post") },
+                        { showPostData(it) },
                         printError("failed to retrieve fullpost"))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        getMenuInflater().inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    private fun showPostData(data: PostData) {
+        val titleView = findViewById(R.id.post_title) as TextView
+        val subtitleView = findViewById(R.id.post_subtitle) as TextView
+        val contentView = findViewById(R.id.post_content) as TextView
+        titleView.setText("Вот те раз!")
+        subtitleView.setText(data.content.author)
+        contentView.setText(data.content.text)
     }
 }
