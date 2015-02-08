42a62aa9bcd49156e93a0c35c6c50ab2629dbf24
Dmitry Suzdalev <dimsuz@gmail.com>
2015-02-08 21:46:00 +0200

    add an utility function

--- app/src/main/kotlin/advaitaworld/parsing/Data.kt
+++ app/src/main/kotlin/advaitaworld/parsing/Data.kt
@@ -5,7 +5,11 @@ import rx.Observable
 public data class ContentInfo(val author: String,
                               val text: CharSequence,
                               val dateString: String,
-                              val rating: String?)
+                              val rating: String?) {
+    fun toShortString(maxTextLength: Int) : String {
+        return "[$author]: ${text.subSequence(0, Math.min(maxTextLength, text.length()))}"
+    }
+}
 
 public data class ShortPostInfo(
         val content: ContentInfo,
