18800da5a391896e5e23e65898ab2a29b30e1905
Dmitry Suzdalev <dimsuz@gmail.com>
2015-02-08 02:34:12 +0200

    Implement recursive and iterative comments parsing
    Recursive crashes, iterative is not tested

--- app/src/main/kotlin/advaitaworld/parsing/FullPostParser.kt
+++ app/src/main/kotlin/advaitaworld/parsing/FullPostParser.kt
@@ -2,6 +2,9 @@ package advaitaworld.parsing
 
 import org.jsoup.Jsoup
 import android.text.Html
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import java.util.Stack
 
 /**
  * Parses a full post
@@ -17,9 +20,64 @@ public fun parseFullPost(stream: java.io.InputStream, baseUri: String) : PostDat
     val voteCountStr = topicContainer.selectFirst(".vote-count > span").text()
     val voteCount = parsePostVoteCount(voteCountStr)
     val contentInfo = ContentInfo(author, Html.fromHtml(content), dateString, voteCount)
-    return PostData(contentInfo, listOf(
-            CommentNode(ContentInfo("Dima", "hello", "date", null), null),
-            CommentNode(ContentInfo("Dima", "hello", "date", null), null),
-            CommentNode(ContentInfo("Dima", "hello", "date", null), null)))
+    return PostData(contentInfo, parseComments(document))
+}
+
+private fun parseComments(document: Document): List<CommentNode> {
+    // select all the top-level comment nodes
+    val nodes = document.select("#comments > .comment-wrapper")
+    if(nodes.isEmpty()) {
+        return listOf()
+    }
+    return nodes.map { parseCommentWrapper(it) }
+}
+
+data class TreeWalkNode(val parent: TreeWalkNode?,
+                        val element: Element,
+                        var discovered: Boolean = false,
+                        var children: MutableList<ContentInfo> = arrayListOf())
+
+private fun parseCommentWrapperIterative(commentWrapper: Element) : CommentNode {
+    // this implements a tree traversal depth-first algorithm
+    // (with slight variation - adding children to parent nodes along the way)
+    val stack : Stack<TreeWalkNode> = Stack()
+    val rootNode = TreeWalkNode(null, commentWrapper)
+    stack.push(rootNode)
+    while(!stack.isEmpty()) {
+        val node = stack.pop()
+        if(!node.discovered) {
+            node.discovered = true
+            if(node.parent != null) {
+                node.parent.children.add(parseComment(node.element))
+            }
+
+            val childElems = node.element.select(".comment-wrapper")
+            for(i in childElems.size()-1 downTo 0) {
+                stack.push(TreeWalkNode(node, childElems.get(i), discovered = false))
+            }
+        }
+    }
+    return toCommentNode(rootNode)
+}
+
+private fun toCommentNode(rootNode: TreeWalkNode) : CommentNode {
+    // todo
+}
+
+fun parseCommentWrapper(commentWrapper: Element): CommentNode {
+    val commentInfo = parseComment(commentWrapper.selectFirst(".comment"))
+    val childCommentWrappers = commentWrapper.select(".comment-wrapper")
+    val childNodes = if(!childCommentWrappers.isEmpty())
+        childCommentWrappers.map { parseCommentWrapper(it) } else null
+    return CommentNode(commentInfo, childNodes)
+}
+
+fun parseComment(commentElem: Element): ContentInfo {
+    val author = commentElem.selectFirst(".comment-author").text()
+    val content = commentElem.selectFirst(".comment-content > .text").html()
+    val dateString = commentElem.selectFirst(".comment-date > time").text()
+    val voteCountStr = commentElem.selectFirst(".vote-count").text()
+    val voteCount = parsePostVoteCount(voteCountStr)
+    return ContentInfo(author, content, dateString, voteCount)
 }
 
