31a7c471e5669253495a4b98aa47e94591cb8e55
Dmitry Suzdalev <dimsuz@gmail.com>
2015-04-18 21:17:08 +0200

    Add the basic auth activity layout

--- app/src/main/kotlin/advaitaworld/AuthActivity.kt
+++ app/src/main/kotlin/advaitaworld/AuthActivity.kt
@@ -7,8 +7,9 @@ import android.support.v7.widget.Toolbar
 public class AuthActivity : RxActionBarActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_authorization)
         setSupportActionBar(findViewById(R.id.toolbar) as Toolbar)
+        setTitle(R.string.login)
         createMainNavigationDrawer(this)
     }
 }
