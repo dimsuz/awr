430d7f0bdfa15a011ff60004385d34eb6c827858
Dmitry Suzdalev <dimsuz@gmail.com>
2015-04-20 00:28:00 +0200

    Draft some login info parsing code

--- app/src/main/kotlin/advaitaworld/parsing/PostFeedParser.kt
+++ app/src/main/kotlin/advaitaworld/parsing/PostFeedParser.kt
@@ -2,7 +2,11 @@ package advaitaworld.parsing
 
 import android.text.Html
 import org.jsoup.Jsoup
+import timber.log.Timber
+import java.io.BufferedReader
 import java.io.Reader
+import java.util.regex.MatchResult
+import java.util.regex.Pattern
 
 fun parsePostFeed(content: String): List<ShortPostInfo> {
     val document = Jsoup.parse(content)
@@ -25,6 +29,39 @@ fun parsePostFeed(content: String): List<ShortPostInfo> {
     return parsedPosts
 }
 
-fun extractLoginInfo(content: Reader) : LoginInfo {
+fun extractLoginInfo(content: Reader, needUserInfo: Boolean) : LoginInfo {
+    val reader = BufferedReader(content)
+    try {
+        val patterns = createMatchPatterns(needUserInfo)
+        val matchResults: MutableMap<String, MatchResult> = hashMapOf()
+        reader.lines()
+            .takeWhile { matchResults.size() != patterns.size() }
+            .forEach { line ->
+                Timber.d("analyzing line $line")
+                for(e in patterns) {
+                    val matcher = e.getValue().matcher(line)
+                    if(matcher.find()) {
+                        // found one, save it,
+                        matchResults.put(e.getKey(), matcher.toMatchResult())
+                        // this line could contain some other pattern, so next for-loop iteration
+                        // will continue from the last spot
+                    }
+                }
+            }
+        return processMatchResults(matchResults)
+    } finally {
+        reader.close()
+    }
+}
+
+private fun createMatchPatterns(needUserInfo: Boolean) : Map<String, Pattern> {
+    val patterns = hashMapOf(
+        "sessionId" to Pattern.compile(""),
+        "securityKey" to Pattern.compile(""))
+    if(needUserInfo) patterns.put("userName", Pattern.compile(""))
+    return patterns
+}
+
+private fun processMatchResults(matchResults: Map<String, MatchResult>): LoginInfo {
     throw UnsupportedOperationException()
 }
