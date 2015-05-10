c5e810e4169a5862786bb078e5b17cf8217cdee8
Dmitry Suzdalev <dimsuz@gmail.com>
2015-05-10 14:36:07 +0200

    Correctly handle situation when <cut> appears early in post

--- app/src/main/kotlin/advaitaworld/parsing/PostFeedParser.kt
+++ app/src/main/kotlin/advaitaworld/parsing/PostFeedParser.kt
@@ -1,5 +1,7 @@
 package advaitaworld.parsing
 
+import android.text.Spanned
+import android.text.style.ClickableSpan
 import org.jsoup.Jsoup
 
 fun parsePostFeed(content: String, mediaResolver: MediaResolver): List<ShortPostInfo> {
@@ -28,19 +30,48 @@ fun parsePostFeed(content: String, mediaResolver: MediaResolver): List<ShortPost
 }
 
 private fun shortenForDisplay(text: CharSequence) : CharSequence {
-    val MAX_LENGTH_CHARS = 1000
-    // FIXME when text contains an image in the first MAX_LENGTH_CHARS, limit to somehting smaller than MAX_LENGTH_CHARS
-    if(text.length() <= MAX_LENGTH_CHARS) {
+    val maxLength = getMaxLength(text)
+    if(text.length() <= maxLength) {
         return text
     }
 
     // note that conversion to String will loose all span info. Anyway, it is not needed for limits calculations.
     // but if needed, will just use original text
-    val limited = text.toString().substring(0, MAX_LENGTH_CHARS)
+    val limited = text.toString().substring(0, maxLength)
     // Not doing any handling of situation when cut happens to be in the middle of some span section:
     // turned out that subSequence already deals with it nicely, be updating the end of that span to
     // be the end of the new string
     return text.subSequence(0, findCutPoint(limited))
+}
+
+// FIXME when text contains an image in the first MAX_LENGTH_CHARS, limit to somehting smaller than MAX_LENGTH_CHARS
+private fun getMaxLength(text: CharSequence): Int {
+    val defaultMaxLength = 1000
+    // Sometimes user can insert 'cut' right in the beginning of the post, resulting in 'Read more'
+    // link appearing earlier than a defaultMaxLength used here. In this case the we will limit
+    // the post summary even more, so that it excludes this ugly (in app world) link
+    val readMoreStartPos = getReadMoreLinkPos(text, defaultMaxLength)
+    return if(readMoreStartPos == -1) defaultMaxLength else readMoreStartPos
+}
+
+/**
+ * Returns a start position of 'Read more ->' link or -1 if it is not found in range [0, endIndex]
+ */
+private fun getReadMoreLinkPos(text: CharSequence, endIndex: Int) : Int {
+    // Find a clickable span with predefined text used by AW
+    // (there can be other valid links which is not of concern here)
+    val readMoreText = "Читать дальше"
+    val spanned = text as Spanned
+    val clickSpans = spanned.getSpans(0, endIndex, javaClass<ClickableSpan>())
+    for(span in clickSpans) {
+        val s = spanned.getSpanStart(span)
+        val e = spanned.getSpanEnd(span)
+        val spanContents = text.subSequence(s, e)
+        if(spanContents.toString().contains(readMoreText)) {
+            return s
+        }
+    }
+    return -1
 }
 
 private fun findCutPoint(s: String) : Int {
