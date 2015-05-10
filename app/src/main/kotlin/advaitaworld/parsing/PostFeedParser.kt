ec5edb6848ee78247172f4e921e87b8b8b64dbe7
Dmitry Suzdalev <dimsuz@gmail.com>
2015-05-10 12:32:23 +0200

    Finish implementation of basic cut insertion

--- app/src/main/kotlin/advaitaworld/parsing/PostFeedParser.kt
+++ app/src/main/kotlin/advaitaworld/parsing/PostFeedParser.kt
@@ -1,7 +1,5 @@
 package advaitaworld.parsing
 
-import android.content.Context
-import android.text.Html
 import org.jsoup.Jsoup
 
 fun parsePostFeed(content: String, mediaResolver: MediaResolver): List<ShortPostInfo> {
@@ -19,9 +17,39 @@ fun parsePostFeed(content: String, mediaResolver: MediaResolver): List<ShortPost
         val postTitle = postTitleElem.text()
         val postLink = postTitleElem.attr("href")
         val postId = parsePostLink(postLink)!!
-        val contentInfo = ContentInfo(author, parseHtmlContent(text, mediaResolver), dateString, voteCount)
+        val parsedPost = parseHtmlContent(text, mediaResolver)
+        val contentInfo = ContentInfo(author, shortenForDisplay(parsedPost), dateString, voteCount)
         ShortPostInfo(postId, postTitle, contentInfo, commentCount)
     })
     return parsedPosts
+}
+
+private fun shortenForDisplay(text: CharSequence) : CharSequence {
+    val MAX_LENGTH_CHARS = 1000
+    // FIXME when text contains an image in the first MAX_LENGTH_CHARS, limit to somehting smaller than MAX_LENGTH_CHARS
+    if(text.length() <= MAX_LENGTH_CHARS) {
+        return text
+    }
+
+    // note that conversion to String will loose all span info. Anyway, it is not needed for limits calculations.
+    // but if needed, will just use original text
+    val limited = text.toString().substring(0, MAX_LENGTH_CHARS)
+    // Not doing any handling of situation when cut happens to be in the middle of some span section:
+    // turned out that subSequence already deals with it nicely, be updating the end of that span to
+    // be the end of the new string
+    return text.subSequence(0, findCutPoint(limited))
+}
+
+private fun findCutPoint(s: String) : Int {
+    // prefer to limit on last sentense rather than chop between words...
+    // try in order: dot, unicode '...', comma, etc, space
+    val seps = ".\u2026!?;:,\n "
+    var cutIdx = -1
+    for(c in seps) {
+        cutIdx = s.lastIndexOf(c)
+        if(cutIdx != -1) break
+    }
+    // if cutIdx == -1 here: who in their right mind would submit a text without any delimeters? oh, well...
+    return if(cutIdx != -1) Math.min(cutIdx+1, s.length()) else s.length()
 }
 
